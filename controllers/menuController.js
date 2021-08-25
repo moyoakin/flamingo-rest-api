c0a1493de6d7b7340bb96fba716af72dd3972ca7
moyoakin <moyoakin1211@gmail.com>
2021-08-24 23:40:13 -0700

    controllers and routes update

--- controllers/menuController.js
+++ controllers/menuController.js
@@ -1,47 +1,67 @@
-import asyncHandler from 'express-async-handler';
+import asyncHandler from "express-async-handler";
 import Menu from "../models/menuModel.js";
 
-const addMenu = asyncHandler(async(req,res)=>{
-    const { itemName, itemDescription,itemPrice, imageUrl } = req.body;
-    
+const addMenu = asyncHandler(async (req, res) => {
+  const { itemName, itemDescription, itemPrice, imageUrl } = req.body;
 
-    const menuExist = await Menu.findOne({itemName: itemName})
+  const menuExist = await Menu.findOne({ itemName: itemName });
 
-    if (menuExist) {
-        res.status(400);
-        throw new Error(`Menu item ${itemName} already exist`);
-      }
+  if (menuExist) {
+    res.status(400);
+    throw new Error(`Menu item ${itemName} already exist`);
+  }
 
-      const savedMenu = await Menu.create({
-        itemName,
-        itemPrice,
-        itemDescription,
-        imageUrl,
-        status:true,
-        dateCreated:Date.now()
+  const savedMenu = await Menu.create({
+    itemName,
+    itemPrice,
+    itemDescription,
+    imageUrl,
+    status: true,
+    dateCreated: Date.now(),
+  });
 
-      })
+  if (savedMenu) {
+    res.status(201).json({
+      _id: savedMenu.id,
+      itemName: savedMenu.itemName,
+      itemPrice: savedMenu.itemPrice,
+      itemDescription: savedMenu.itemDescription,
+      imageUrl: savedMenu.imageUrl,
+    });
+  } else {
+    result.status(400);
+    throw new Error("item does not exist");
+  }
+});
+
+const findItemByID = asyncHandler(async (req, res) => {
+  const menuItem = await Menu.findById(req.params.id);
+
+  if (menuItem) {
+    res.status(201).json(menuItem);
+  } else {
+    res.status(400);
+    throw new Error("Invalid  Menu ID");
+  }
+});
+
+const deleteMenu = asyncHandler(async (req, res) => {
+  const menuItem = await Menu.findById(req.params.id);
+
+  if (menuItem) {
+    menuItem.status = false;
+    menuItem.dateDeleted = Date.now();
+    res.status(200).json({ message: "Menu Deleted" });
+  } else {
+    res.status(400).json({message: "Menu not found"});
+  }
+});
+
+const getAllMenu = asyncHandler(async (req, res) => {
+  const allMenu = await Menu.find({ status: true });
+  res.json(allMenu);
+});
+
+export { addMenu, findItemByID, deleteMenu, getAllMenu };
 
 
-      if(savedMenu){
-          res.status(201).json({
-              _id:savedMenu.id,
-              itemName: savedMenu.itemName,
-              itemPrice: savedMenu.itemPrice,
-              itemDescription: savedMenu.itemDescription,
-              imageUrl: savedMenu.imageUrl
-
-          })
-      }else{
-          result.status(400)
-          throw new Error("item does not exist")
-      }
-
-})
-
-
-
-export {addMenu} 
-
-// get all menu
-// delete by ID
--- route/menuRoutes.js
+++ route/menuRoutes.js
@@ -1,10 +1,18 @@
 import express from "express";
 
-import { addMenu } from "../controllers/menuController.js";
+import { addMenu, findItemByID, deleteMenu,getAllMenu } from "../controllers/menuController.js";
 import { protect } from "../middleware/authMiddleware.js";
 
 const menuRoutes = express.Router();
 
 menuRoutes.route("/create").post(protect, addMenu);
+menuRoutes.route("/").get(getAllMenu);
+
+
+menuRoutes
+.route("/:id")
+.get(protect, findItemByID)
+.delete(protect, deleteMenu)
+
 
 export default menuRoutes;
